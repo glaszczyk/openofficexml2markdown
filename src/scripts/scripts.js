5cde38c910dbfe9b88f79316eb215b07c2050234
glaszczyk <info@grzegorzlaszczyk.pl>
2017-03-18 09:33:57 +0100

    Traversing XML DOM

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,11 +1,11 @@
-//Odczytujemy pliki XML z serwera przez XMLHttpRequest
-//Dla testów przygotowane są trzy pliki, przypisane do urlOne, urlTwo, urlThee
+// Read XML files form server via XMLHttpRequest
+// For tests purposes three different sources urlOne, urlTwo, urlThee defined
 const xhr = new XMLHttpRequest(),
 	urlOne = 'http://blog.grzegorzlaszczyk.pl/tests/content-one-para.xml',
 	urlTwo = 'http://blog.grzegorzlaszczyk.pl/tests/content-para-char.xml',
 	urlThree = 'http://blog.grzegorzlaszczyk.pl/tests/content-styled-unstyled.xml';
 
-//zmieniamy zmienne odpowiadające za url poszczególnych plików
+// change url here
 xhr.open( 'GET', urlOne, true );
 xhr.send( null );
 xhr.onload = success;
@@ -14,22 +14,42 @@ xhr.onerror = error;
 function success( event ) {
 	if ( xhr.status == 200 ) {
 		screenOutput( xhr.responseText );
-		consoleOutput( xhr.responseXML.childNodes[ 0 ]);
+		logger( children( xhr.responseXML )[ 0 ]);
+		traverseDOM( 'office:text' );
 	} else {
-		console.log( xhr.statusText );
+		logger( xhr.statusText );
 	}
 }
 function error( event ) {
-	console.log( 'We have an error here' );
+	logger( 'We have an error here' );
 }
-//Wyświetlamy zawartość pliku XML na ekran
+// return node.childNodes
+function children( node ) {
+	return Array.from( node.childNodes );
+}
+// Display content to console
+function logger( content ) {
+	console.log( content );
+}
+// Display XML content as part of DOM and replace <p> in index.html
 function screenOutput( content ) {
 	let container = document.querySelector( '.xml-content' );
 	container.outerHTML = content;
 }
-//Wyświetlamy zawartość pliku XML do konsoli
-function consoleOutput( content ) {
-	console.log( content );
+// Read content from node given
+function traverseDOM( tag ) {
+	const tagContent = children( document.getElementsByTagName( tag )[ 0 ]);
+	logger( tagContent );
+	logger( tagContent.map( element => readFromFirst( children( element ))));
 }
-
-
+// Read nodes starting from highest child
+function readFromFirst( node ) {
+	return node.reduce(( prev, current ) => {
+		if ( current.nodeType === 1 ) {
+			return ( prev + readFromFirst( children( current )));
+		} else if ( current.nodeType === 3 ) {
+			logger( current.nodeValue );
+			return ( prev + `<${current.parentElement.attributes[ 0 ].nodeValue}>` + current.nodeValue );
+		}
+	}, '' );
+}
