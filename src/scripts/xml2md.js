15b0c25c2a8939c58a7e266468b8339a6a9c1dd4
glaszczyk <info@grzegorzlaszczyk.pl>
2017-03-26 22:34:52 +0200

    Read nodes with node.js added

--- src/scripts/xml2md.js
+++ src/scripts/xml2md.js
@@ -4,10 +4,11 @@ const fs = require( 'fs' ),
 	util = require( 'util' ),
 	urlOne = '../../TestSamples/OnePara/content-one-para.xml',
 	urlTwo = '../../TestSamples/ParaCharStylesAndDefault/content-para-char.xml',
-	urlThree = '../../TestSamples/StyledAndUnstyledText/content-styled-unstyled.xml';
+	urlThree = '../../TestSamples/StyledAndUnstyledText/content-styled-unstyled.xml',
+	urlFour = '../../TestSamples/ParasAndLists/content-paras-lists.xml';
 
 //fs.rename('OnePara.odt', 'OnePara.zip');
-
+/*
 function nodeInfo( element ) {
 	console.log( `nodeName >> ${element.nodeName}` );
 	console.log( `nodeValue >> ${element.nodeValue}` );
@@ -28,17 +29,49 @@ function children( node ) {
 	}
 
 }
-
+*/
 jsdom.env({
-	file: urlOne,
+	file: urlFour,
 	parsingMode: 'xml',
 	done( error, window ) {
 		if ( error ) {
 			console.log( error );
 		}
 		const nodeWindow = window,
-			myXML = Array.from( nodeWindow.document.getElementsByTagName( 'office:text' ));
-		console.log( myXML[ 0 ].innerHTML );
-		myXML.map( node => children( node ));
+			response = nodeWindow.document,
+			content = getContent( response, 'office:text' );
+		traverseDOM( content );
 	}
 });
+// get content from node tag
+function getContent( content, tagName ) {
+	const childs = children( content ),
+		element = childs.reduce(( prev, current ) => {
+			return ( current.nodeName === tagName ) ? current : getContent( current, tagName );
+		}, '' );
+	return element;
+}
+// return node.childNodes
+function children( node ) {
+	return Array.from( node.childNodes );
+}
+// Read content from node given
+function traverseDOM( content ) {
+	const contentToRead = children( content );
+	console.log( contentToRead.map( element => readFromFirst( children( element ))));
+}
+// Read nodes starting from highest child
+function readFromFirst( node ) {
+	return node.reduce(( prev, current ) => {
+		if ( current.nodeType === 1 ) {
+			return ( prev + readFromFirst( children( current )));
+		} else if ( current.nodeType === 3 ) {
+			//console.log( current.nodeValue );
+			if ( current.parentElement.attributes.length === 0 ) {
+				return ( prev + `<${current.parentElement.nodeName}>` + current.nodeValue );
+			} else {
+				return ( prev + `<${current.parentElement.attributes[ 0 ].nodeValue}>` + current.nodeValue );
+			}
+		}
+	}, '' );
+}
